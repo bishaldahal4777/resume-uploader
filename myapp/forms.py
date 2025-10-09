aee728758d292dce37655c02d590d3186849af57
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-09 08:56:17 +0545

    form label

--- myapp/forms.py
+++ myapp/forms.py
@@ -6,3 +6,4 @@ class ResumeForm(forms.ModelForm):
     class Meta:
         model = Resume
         fields = ("name","dob","gender","locality","city","pin","state","mobile","email","job_city","profile_image","my_file")
+        labels = {'name':'Full Name', 'dob':'Date of Birth', 'pin':'pin code', 'mobile': 'Contact Number', 'email':'Email Address','profile_image':'Profile Image','my_file':'Document'}
